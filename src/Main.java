c01b849cff822268c5210927f02e6836c33088c0
Laxmiraj180 <142928712+Laxmiraj180@users.noreply.github.com>
2026-04-14 23:59:38 +0530

    Add Main class to display current date

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Welcome to Dinesh Sathe YTB Channel ! Current date: " + currentDate);
+    }
+}
